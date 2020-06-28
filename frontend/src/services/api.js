a080de46e182b6693e3f09b319b4be5cf6192f9e
alissonldacunha <alissonlimadacunha@hotmail.com>
2020-06-28 11:56:53 -0300

    styles-movies

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const urlImage = "https://raw.githubusercontent.com/alissonldacunha/api-movie-crud/master/uploads/";
+export const urlImage = "https://raw.githack.com/alissonldacunha/api-movie-crud/master/uploads/";
 
 const api = axios.create({
   baseURL: 'http://localhost:3333/'
