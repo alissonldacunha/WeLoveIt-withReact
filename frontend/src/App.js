37b655d6e1864ab21479464392505e18c46c52e0
alissonldacunha <alissonlimadacunha@hotmail.com>
2020-06-27 21:29:14 -0300

    movie-list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import api from './services/api';
 
 import './global.css';
 
 import Header from './components/Header';
-import MovieList from './components/Movies';
+import MovieList from './pages/Movies'
 
 const App = () => (
   <div className="App">
---
+++ frontend/src/pages/Movies/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+export default class MovieList extends Component {
+  state = {
+    movies: [],
+  }
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/movies');
+
+    this.setState({ movies: response.data });
+  };
+
+  render() {
+    return (
+      <div className="container_films">
+        {this.state.movies.map(movies => (
+          <ul className="films-list" key={movies.id}>
+            <li className="films-list-item">
+              <figure className="poster-film">
+                <img src={movies.image} alt="" />
+                <div className="film-info">
+                  <h3>{movies.title}</h3>
+                  <a href="#" className="film_link">
+                    <i className="fa fa-play" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </figure>
+
+              <div className="resume">
+                <h3 className="film-resume-title">
+                  {movies.title}
+                </h3>
+                <p className="genero">
+                  <a href="#" className="film_link">{movies.categories}</a>
+                </p>
+                <p className="filme-nota">
+                  <i className="" aria-hidden="true"></i> {movies.note}
+                </p>
+              </div>
+            </li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
+}
